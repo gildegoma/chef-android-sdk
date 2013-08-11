792961550eb25ec3fd5ded56f2330496b42f1d6d
Gilles Cornu <git@gilles.cornu.name>
2013-08-11 23:42:14 +0200

    Bump to next version 0.0.1
    [ci skip]

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Gilles Cornu"
 maintainer_email "foss@gilles.cornu.name"
 license          "Apache 2.0"
 description      "Installs Google Android SDK"
-version          "0.0.0" # draft mode
+version          "0.0.1" # draft mode
 
 %w{ java ark }.each do |dep|
   depends dep

e18595f3fd50ff740390ae2f44ab6818aaf38b0a
Gilles Cornu <git@gilles.cornu.name>
2014-03-31 22:39:44 +0200

    Update Credits, etc.

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,15 +5,18 @@ default['android-sdk']['setup_root']     = nil  # ark defaults (/usr/local) is u
 
 default['android-sdk']['version']        = '22.6.2'
 default['android-sdk']['checksum']       = 'f87e1bb207783a8b7d59775dcd3a8a2828034787860e44c99a9aefebdf7cb28f'
-
 default['android-sdk']['download_url']   = "http://dl.google.com/android/android-sdk_r#{node['android-sdk']['version']}-linux.tgz"
 
 #
 # List of Android SDK components to preinstall:
-# Selection based on 
-# - Platform usage statistics (see http://developer.android.com/about/dashboards/index.html) 
+# Selection based on
+# - Platform usage statistics (see http://developer.android.com/about/dashboards/index.html)
 # - Build Tools releases: http://developer.android.com/tools/revisions/build-tools.html
 #
+# Hint:
+# Add 'tools' to the list below if you wish to get the latest version,
+# without having to adapt 'version' and 'checksum' attributes of this cookbook.
+# Note that it will require (waste) some extra download effort.
 default['android-sdk']['components']     = %w(platform-tools
                                               build-tools-19.0.3
                                               android-19

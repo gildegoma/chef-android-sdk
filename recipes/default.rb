bf6465c007ed27721951af1b6303fefaa13869ba
Gilles Cornu <git@gilles.cornu.name>
2014-06-27 11:27:29 +0200

    Change basic idempotency guard
    prefer checking 'temp' folder, in case no 'build-tools' are
    preinstalled.
    Still waiting for #12

--- recipes/default.rb
+++ recipes/default.rb
@@ -103,7 +103,7 @@ package 'expect'
 #
 
 # KISS: use a basic idempotent guard, waiting for https://github.com/gildegoma/chef-android-sdk/issues/12
-unless File.exist?("#{setup_root}/#{node['android-sdk']['name']}-#{node['android-sdk']['version']}/build-tools")
+unless File.exist?("#{setup_root}/#{node['android-sdk']['name']}-#{node['android-sdk']['version']}/temp")
 
   # With "--filter node['android-sdk']['components'].join(,)" pattern,
   # some system-images were not installed as expected.

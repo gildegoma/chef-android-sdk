533031ed2fb67417f8d289e36b9a5f95ee8a125e
Dennis Hoer <dennis.hoer@pearson.com>
2015-07-09 08:55:04 -0700

    Update sdk to v24.3.3 along with components

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,8 +3,8 @@ default['android-sdk']['owner']          = 'root'
 default['android-sdk']['group']          = 'root'
 default['android-sdk']['setup_root']     = nil  # ark defaults (/usr/local) is used if this attribute is not defined
 
-default['android-sdk']['version']        = '23'
-default['android-sdk']['checksum']       = '2eaab06852ac21b6c79df73c07a667c5da5be57f7ffcbd4f17aef7efeea22ac1'
+default['android-sdk']['version']        = '24.3.3'
+default['android-sdk']['checksum']       = 'a05023aaf149d40a0a848ad49d5c6b43ec730443efb89d1dfa584a132a642bdf'
 default['android-sdk']['download_url']   = "http://dl.google.com/android/android-sdk_r#{node['android-sdk']['version']}-linux.tgz"
 
 #
@@ -18,7 +18,11 @@ default['android-sdk']['download_url']   = "http://dl.google.com/android/android
 # without having to adapt 'version' and 'checksum' attributes of this cookbook.
 # Note that it will require (waste) some extra download effort.
 default['android-sdk']['components']     = %w(platform-tools
-                                              build-tools-20.0.0
+                                              build-tools-22.0.1
+                                              android-22
+                                              sys-img-armeabi-v7a-android-22
+                                              android-21
+                                              sys-img-armeabi-v7a-android-21
                                               android-20
                                               sys-img-armeabi-v7a-android-wear-20
                                               android-19
